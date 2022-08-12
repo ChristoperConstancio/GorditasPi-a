2d1b896349ade1005a748cb8007ed253cdf2a65b
christoper constancio <chris_constancio@hotmail.com>
2022-08-12 11:17:09 -0500

    second version

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,22 @@ module.exports = {
             '0 35px 35px rgba(0, 0, 0, 0.25)',
             '0 45px 65px rgba(0, 0, 0, 0.15)'
         ]
+      },
+      screens: {
+        'sm': '300px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
       }
     },
   },
